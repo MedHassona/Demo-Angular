16237e516037817e860292cabaeb6ef26dfc6997
med hassona <mhossona@gmail.com>
2019-02-13 12:29:53 +0100

    finished exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Demo-Angular';
+
+  //post1: Post = new Post("Mon premier post","i love real madrid and i will support it for ever <3");
+  //post2: Post = new Post("Mon deuxième post","i have been in sudan once, it is an amazing arab country, people there are very kind and loveable :)");
+  //post3: Post = new Post("Encore un post","i just keep talking talkin °.* someone stop me :D");
+ 
+  posts = [
+    {
+      title : 'Mon premier post',
+      content : 'i love real madrid and i will support it for ever <3',
+      loveIts : 0
+    },
+    {
+      title : 'Mon deuxième post',
+      content  : 'i have been in sudan once, it is an amazing arab country, people there are very kind and loveable :)',
+      loveIts : 0
+    },
+    {
+      title : 'Encore un post',
+      content : 'i just keep talking talkin °.* someone stop me :D',
+      loveIts : 0
+    }
+  ];
+  
+
+  constructor() {
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
